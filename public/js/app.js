10347c8e95cd06e7391167810cd28a051f4db283
Alex Kufel <kufelalexander@gmail.com>
2019-05-16 19:31:19 +0300

    Added fetch func in js/app.js to grab wether information from internet and show in console for user

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
 console.log('Client side js file is loaded')
+
+fetch('http://localhost:3000/weather?address=minsk').then((response) => {
+    response.json().then((data) => {
+        // console.log(data)
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+        
+        
+    })
+})
