e117472bfcd8c505a3af1550ce30c51ded50d84a
Alex Kufel <kufelalexander@gmail.com>
2019-05-20 07:52:08 +0300

    Setup app for Heroki

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,7 @@ weatherFofm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    //123
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
